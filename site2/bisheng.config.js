51b87ccff054e47ae7e86f6ffbdc29c6b29a030e
nowgoant <majun1@jd.com>
2017-07-04 16:12:40 +0800

    update

---
+++ site2/bisheng.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
+
+module.exports = {
+  port: 9000,
+  source: {
+    posts: './posts',
+  },
+  exclude: /should-be-ignore/,
+  theme: './site2/theme',
+  htmlTemplate: './site2/theme/static/template.html',
+  themeConfig: {
+    home: '/',
+    sitename: 'One',
+    tagline: 'The one theme for bisheng',
+    // navigation: [{
+    //   title: 'BiSheng',
+    //   link: 'https://github.com/benjycui/bisheng',
+    // }],
+    // footer: 'Copyright and so on...',
+    // hideBisheng: true,
+    github: 'https://github.com/benjycui/bisheng',
+  }
+};
---
+++ site2/theme/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  lazyLoad: true,
+  pick: {
+    posts(markdownData) {
+      return {
+        meta: markdownData.meta,
+        description: markdownData.description,
+      };
+    },
+  },
+  plugins: [
+    'bisheng-plugin-description',
+    'bisheng-plugin-toc?maxDepth=2&keepElem',
+    'bisheng-plugin-antd',
+    'bisheng-plugin-react?lang=__react',
+  ],
+  routes: [{
+    path: '/',
+    component: './template/Archive',
+  }, {
+    path: '/posts/:post',
+    component: './template/Post',
+  }, {
+    path: '/tags',
+    component: './template/TagCloud',
+  }],
+};
